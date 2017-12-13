ffb1f969f72704f81f2b85719bdb4fca29d3129d
a fool <afoolsbag@gmail.com>
2017-12-13 17:33:24 +0800

    feat(rrcxxlang): chaotic...

---
+++ rrcxxlang/src/cxx_standard_version.hpp
@@ -0,0 +1,67 @@
+/** \file
+ *  \brief C++ 标准版本。
+ *  \sa [Pre-defined C/C++ Compiler Macros](https://sourceforge.net/p/predef/wiki/Compilers/).
+ *  \sa [C++ Standards Support in GCC](https://gcc.gnu.org/projects/cxx-status.html).
+ *  \sa [Support For C++11/14/17 Features (Modern C++)](https://msdn.microsoft.com/library/hh567368.aspx).
+ *  \author zhengrr
+ *  \date 2017-12-13
+ *  \copyright The MIT License
+ */
+#ifndef RRCXXLANG_CXX_STANDARD_VERSION_HPP_
+#define RRCXXLANG_CXX_STANDARD_VERSION_HPP_
+
+/* Language Standards */
+
+#ifdef __cplusplus
+# define CXX_STD_VER __cplusplus
+#else
+# error "__cplusplus NOT DEFINED"
+#endif
+
+#define CXX_STD_17 201703L
+#define CXX_STD_14 201402L
+#define CXX_STD_11 201103L
+#define CXX_STD_98 199711L
+
+/* GCC C/C++ */
+
+#ifdef __GNUC__
+# ifndef __GNUC_PATCHLEVEL__
+#  define __GNUC_PATCHLEVEL__ 0
+# endif
+# define CXX_GCC_VER (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
+#else
+# define CXX_GCC_VER 0
+#endif
+
+#define CXX_GCC_7     70000
+#define CXX_GCC_4_8_1 40801
+#define CXX_GCC_4_4   40300
+#define CXX_GCC_4_3   40300
+
+/* Microsoft Visual C++ */
+
+#ifdef _MSC_VER
+# define CXX_MVC_VER _MSC_VER
+#else
+# define CXX_MVC_VER 0
+#endif
+
+#define CXX_MVC_2017 1910
+#define CXX_MVC_2015 1900
+#define CXX_MVC_2013 1800
+#define CXX_MVC_2012 1700
+#define CXX_MVC_2010 1600
+#define CXX_MVC_2008 1500
+#define CXX_MVC_2005 1400
+#define CXX_MVC_2003 1310
+#define CXX_MVC_7_0  1300
+#define CXX_MVC_6_0  1200
+#define CXX_MVC_5_0  1100
+#define CXX_MVC_4_2  1020
+#define CXX_MVC_4_0  1000
+#define CXX_MVC_3_0   900
+#define CXX_MVC_1_0   800
+
+
+#endif// RRCXXLANG_CXX_STANDARD_VERSION_HPP_
--- rrcxxlang/src/main.cpp
+++ rrcxxlang/src/main.cpp
@@ -1,30 +1,28 @@
 /** \file
- *  \brief 程序入口。
- *  \sa <http://en.cppreference.com/w/cpp/language/main_function>
+ *  \brief 主函数。
+ *  \sa [Main function](http://en.cppreference.com/w/cpp/language/main_function).
  *  \author zhengrr
- *  \date 2016-10-12 – 11-14
+ *  \date 2016-10-12 – 2017-12-13
  *  \copyright The MIT License
  */
 #include <iostream>
 
-#ifdef ENTRY_SWITCH
-/// 程序入口。
-/** \param argc 参数计数。
- *  \param argv 参数向量。
- *  \return 退出状态。
+/*
+ * 议题：在 main 函数中，使用 return、exit()、还是 abort()
+ *
+ * 　　使用 exit() 将不会触发函数中局部对象的析构, 但静态对象与全局对象不受影响；
+ * 　　使用 abort() 则不会触发任何对象的析构，包括局部、静态、和全局对象。
+ *
+ * 参见：<http://stackoverflow.com/questions/461449>
  */
-int main(int argc, char* argv[])
+
+int altmain(int argc, char *argv[])
 {
     std::cout << "hello, world" << std::endl;
 
-    /*
-     * 议题：main 函数中，return、exit()、还是 abort()
-     *
-     * 　　使用 exit() 将不会触发函数中局部对象的析构, 但静态对象与全局对象不受影响；
-     * 　　使用 abort() 则不会触发任何对象的析构，包括局部、静态、和全局对象。
-     *
-     * 另见：<http://stackoverflow.com/questions/461449>
-     */
     return 0;
 }
-#endif// ENTRY SWITCH
+
+#ifdef RRCXXLANG_MAIN_CPP_
+int main(int argc, char* argv[]) { return altmain(argc, argv); }
+#endif
