39468d487c58241d1506d77a182b5f4576fca1a5
a fool <afoolsbag@gmail.com>
2018-10-11 10:23:14 +0800

    refactor(rrCMake): 更名

---
+++ CMake/Modules/rrCMake.cmake
@@ -0,0 +1,25 @@
+#            _____ ___  ___      _
+#           /  __ \|  \/  |     | |
+#  _ __ _ __| /  \/| .  . | __ _| | _____
+# | '__| '__| |    | |\/| |/ _` | |/ / _ \
+# | |  | |  | \__/\| |  | | (_| |   <  __/
+# |_|  |_|   \____/\_|  |_/\__,_|_|\_\___| rrCMake by FIGlet doom
+# zhengrr
+# The Unlicense
+
+include_guard()
+
+#.rst:
+# rrCMake
+# -------
+#
+# zhengrr's CMake Utilities.
+
+function(rrcmake_include_module_files)
+  file(GLOB zModuleFiles "${CMAKE_CURRENT_LIST_DIR}/rrCMake/*.cmake")
+  foreach(sModuleFile IN LISTS zModuleFiles)
+    include("${sModuleFile}")
+  endforeach()
+endfunction()
+
+rrcmake_include_module_files()
