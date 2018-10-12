0745eda52146750251f61b6d6861ff9c47da24cd
a fool <afoolsbag@gmail.com>
2018-10-12 17:00:15 +0800

    feat(rrCMake): 破坏性地变更 check_name_* 函数的调用方式

--- CMake/Modules/rrCMake/CkName.cmake
+++ CMake/Modules/rrCMake/CkName.cmake
@@ -1,5 +1,5 @@
 # zhengrr
-# 2017-12-18 – 2018-10-11
+# 2017-12-18 – 2018-10-12
 # The Unlicense
 
 include_guard()
@@ -13,16 +13,14 @@ cmake_policy(SET CMP0054 NEW) #3.1+
 #   检查输入是否符合 C 语言标识符命名规则（C identifier rules），
 #   仅包含拉丁字母、阿拉伯数字和下划线，且首字符不为数字::
 #
-#     check_name_with_c_rules(<name> <result-variable>)
-function(check_name_with_c_rules _NAME _RESULT_VARIABLE)
+#     check_name_with_c_rules(<name> <STATUS|WARNING|AUTHOR_WARNING|SEND_ERROR|FATAL_ERROR|DEPRECATION>)
+function(check_name_with_c_rules _NAME _MODE)
   if(NOT ${ARGC} EQUAL 2)
     message(FATAL_ERROR "Incorrect number of arguments: ${ARGN}.")
   endif()
 
-  if("${_NAME}" MATCHES "^[A-Z_a-z]+[0-9A-Z_a-z]*$")
-    set(${_RESULT_VARIABLE} TRUE  PARENT_SCOPE)
-  else()
-    set(${_RESULT_VARIABLE} FALSE PARENT_SCOPE)
+  if(NOT "${_NAME}" MATCHES "^[A-Z_a-z]+[0-9A-Z_a-z]*$")
+    message(${_MODE} "The name isn't meet C identifier rules: ${_NAME}.")
   endif()
 endfunction()
 
@@ -32,16 +30,14 @@ endfunction()
 #   检查输入是否符合 CMake 推荐变量命名规则（CMake recommend variable rules），
 #   仅包含拉丁字母、阿拉伯数字、下划线和连字符::
 #
-#     check_name_with_cmake_rules(<name> <result-variable>)
-function(check_name_with_cmake_rules _NAME _RESULT_VARIABLE)
+#     check_name_with_cmake_rules(<name> <STATUS|WARNING|AUTHOR_WARNING|SEND_ERROR|FATAL_ERROR|DEPRECATION>)
+function(check_name_with_cmake_rules _NAME _MODE)
   if(NOT ${ARGC} EQUAL 2)
     message(FATAL_ERROR "Incorrect number of arguments: ${ARGN}.")
   endif()
 
-  if("${_NAME}" MATCHES "^[-0-9A-Z_a-z]+$")
-    set(${_RESULT_VARIABLE} TRUE  PARENT_SCOPE)
-  else()
-    set(${_RESULT_VARIABLE} FALSE PARENT_SCOPE)
+  if(NOT "${_NAME}" MATCHES "^[-0-9A-Z_a-z]+$")
+    message(${_MODE} "The name isn't meet CMake recommend variable rules: ${_NAME}.")
   endif()
 endfunction()
 
@@ -51,15 +47,13 @@ endfunction()
 #   检查输入是否符合文件扩展名命名规则（file extension rules），
 #   首字符为下脚点，扩展名仅包含拉丁字母和阿拉伯数字，可以串接::
 #
-#     check_name_with_fext_rules(<name> <result-variable>)
-function(check_name_with_fext_rules _NAME _RESULT_VARIABLE)
+#     check_name_with_fext_rules(<name> <STATUS|WARNING|AUTHOR_WARNING|SEND_ERROR|FATAL_ERROR|DEPRECATION>)
+function(check_name_with_fext_rules _NAME _MODE)
   if(NOT ${ARGC} EQUAL 2)
     message(FATAL_ERROR "Incorrect number of arguments: ${ARGN}.")
   endif()
 
-  if("${_NAME}" MATCHES "^([.][0-9A-Za-z]+)+$")
-    set(${_RESULT_VARIABLE} TRUE  PARENT_SCOPE)
-  else()
-    set(${_RESULT_VARIABLE} FALSE PARENT_SCOPE)
+  if(NOT "${_NAME}" MATCHES "^([.][0-9A-Za-z]+)+$")
+    message(${_MODE} "The name isn't meet file extension rules: ${_NAME}.")
   endif()
 endfunction()
