72996c13e8c24e43f75ff7e729263cbf12c038f1
zhengrr <afoolsbag@gmail.com>
2019-03-21 09:13:52 +0800

    feat(rrBoost): 加入 stacktrace 示例

---
+++ rrBoost/stacktrace_test.cpp
@@ -0,0 +1,32 @@
+//===-- Stacktrace ----------------------------------------------*- C++ -*-===//
+///
+/// \file
+/// \brief Stacktrace
+/// \sa <https://boost.org/doc/libs/1_69_0/doc/html/stacktrace.html>
+///
+/// \version 2019-03-21
+/// \since 2019-03-12
+/// \authors zhengrr
+/// \copyright Unlicense
+///
+//===----------------------------------------------------------------------===//
+
+#include <csignal>
+#include <iostream>
+
+#include <boost/filesystem.hpp>
+#include <boost/stacktrace.hpp>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+namespace stacktrace = boost::stacktrace;
+
+namespace rrboost {
+
+TEST(stacktrace, getting_started)
+{
+    cout << stacktrace::stacktrace() << '\n';
+}
+
+}//namespace rrboost
