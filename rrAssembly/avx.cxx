2807b21a23b58152086b8f3a08309ac79a88fb1f
zhengrr <afoolsbag@gmail.com>
2019-10-16 17:52:04 +0800

    feat: 新增汇编示例工程

---
+++ rrAssembly/avx.cxx
@@ -0,0 +1,18 @@
+//===- Advanced Vector Extensions -------------------------------*- C++ -*-===//
+///
+/// \file
+/// \brief 高级向量扩展指令集
+/// sa <https://software.intel.com/articles/introduction-to-intel-advanced-vector-extensions/>
+///
+/// \version 2019-10-15
+/// \since 2019-10-15
+/// \authors zhengrr
+/// \copyright Unlicense
+///
+//===----------------------------------------------------------------------===//
+
+#include <gtest/gtest.h>
+
+namespace rrassembly {
+
+}
