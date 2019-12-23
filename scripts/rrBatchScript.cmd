1263cbbadb1147915c392e5042e186e026a00257
zhengrr <afoolsbag@gmail.com>
2019-12-23 21:05:32 +0800

    feat(scripts): 更新 rrBatchScript.cmd

--- scripts/rrBatchScript.cmd
+++ scripts/rrBatchScript.cmd
@@ -5,61 +5,109 @@
 :: | |  | |  | |_/ / (_| | || (__| | | /\__/ / (__| |  | | |_) | |_
 :: |_|  |_|  \____/ \__,_|\__\___|_| |_\____/ \___|_|  |_| .__/ \__|
 :: zhengrr                                               | |
-:: 2019-09-10 – 2019-09-17                               |_|
+:: 2019-09-10 – 2019-12-23                               |_|
 :: Unlicense
-
+::
 :: see also: https://steve-jansen.github.io/guides/windows-batch-scripting/
 ::           https://wikibooks.org/wiki/Windows_Batch_Scripting
 ::           https://ss64.com/nt/
 
-:: 沉默，除非显式指定，否则不回显指令
+:: "关闭后续命令的默认回显，前缀 @ 用以关闭此行命令自身的默认回显"
 @ECHO OFF
 
-:: 切换字符编码 Change Code Page
+:: "切换字符编码（Change Code Page）到代码页 65001，即 UTF-8"
 CHCP 65001
 
-:: 注释
-REM 官方提供的注释方式 Remark
-::  或者使用两次标签操作，在大多数情形下可以用作注释，且更具可读性
+REM "官方提供的注释方式（Remark）"
+:: "或者使用两次标签操作，在大多数情形下可以用作注释，且更具可读性"
+:: "含有非 ASCII 字符的注释若被误识别为命令，可将其用引号括起以缓解此问题"
 
-:: 全局变量
-SET GLOBAL_VARIABLE=this is a global string.
+:: "设置默认环境中的环境变量，一般由命令提示符作为默认环境"
+:: "环境变量名对大小写不敏感，全大写以表明其作用范围是默认范围，在离开此脚本后依然有效"
+SET COMMAND_PROMPT_ENVIRONMENT_VARIABLE=This is a string save on command prompt.
 
-:: 局部变量
+:: "新压入一层环境，最大支持嵌套 32 层环境"
+:: "后续设置的环境变量都将在新压入的这一层，在 ENDLOCAL 或离开此脚本后这一层将被弹出，这些环境变量也将失效"
 SETLOCAL ENABLEEXTENSIONS
-SET local_variable=this is a local string.
+:: "利用这一特性可以将这些环境变量视为局部变量"
+SET var=This is a string save on local.
 
-:: 特殊变量
+
+:: "实参变量"
+:: see also: https://ss64.com/nt/syntax-args.html
 ECHO.
-ECHO Script fully path: %~f0%
-ECHO Script directory:  %~dp0%
-ECHO Script name:       %~nx0%
-ECHO Script callee:     %0%
-ECHO Argument1:         %1%
-ECHO Argument2:         %2%
-ECHO Argument3:         %3%
-ECHO Argument4:         %4%
-ECHO Argument5:         %5%
-ECHO Argument6:         %6%
-ECHO Argument7:         %7%
-ECHO Argument8:         %8%
-ECHO Argument9:         %9%
+ECHO 提供以下实参变量：
+ECHO.
+ECHO %%0                             %0
+ECHO %%~f0 (fully)                   %~f0
+ECHO %%~d1 (drive)                   %~d0
+ECHO %%~p0 (path)                    %~p0
+ECHO %%~n0 (name)                    %~n0
+ECHO %%~x0 (extension)               %~x0
+ECHO %%*                             %*
+ECHO %%1                             %1
+ECHO %%2                             %2
+ECHO %%3                             %3
+ECHO %%4                             %4
+ECHO %%5                             %5
+ECHO %%6                             %6
+ECHO %%7                             %7
+ECHO %%8                             %8
+ECHO %%9                             %9
 
-SET script_directory=%~dp0%
-SET script_name=%~nx0%
+:: "动态变量"
+:: see also: CMD> SET /?
+ECHO.
+ECHO 若启用了命令提示符扩展（SETLOCAL /ENABLEEXTENSIONS），则提供以下动态变量：
+ECHO.
+ECHO %%CD%%                           %CD%
+ECHO %%DATE%%                         %DATE%
+ECHO %%TIME%%                         %TIME%
+ECHO %%RANDOM%%                       %RANDOM%
+ECHO %%ERRORLEVEL%%                   %ERRORLEVEL%
+ECHO %%CMDEXTVERSION%%                %CMDEXTVERSION%
+ECHO %%CMDCMDLINE%%                   %CMDCMDLINE%
+ECHO %%HIGHESTNUMANODENUMBER%%        %HIGHESTNUMANODENUMBER%
 
-:: 判断指令成功与否
+:: "环境变量"
+:: see also: CMD> SET
+ECHO.
+ECHO 提供以下环境变量：
+ECHO.
+ECHO %%SYSTEMDRIVE%%                  %SYSTEMDRIVE%
+ECHO %%SYSTEMROOT%%                   %SYSTEMROOT%
+ECHO %%WINDIR%%                       %WINDIR%
+ECHO %%DRIVERDATA%%                   %DRIVERDATA%
+ECHO %%PROGRAMFILES%%                 %PROGRAMFILES%
+ECHO %%PROGRAMFILES(x86)%%            %PROGRAMFILES(x86)%
+ECHO %%COMMONPROGRAMFILES%%           %COMMONPROGRAMFILES%
+ECHO %%COMMONPROGRAMFILES(x86)%%      %COMMONPROGRAMFILES(x86)%
+ECHO %%PROGRAMDATA%%                  %PROGRAMDATA%
+ECHO %%HOMEDRIVE%%                    %HOMEDRIVE%
+ECHO %%ALLUSERSPROFILE%%              %ALLUSERSPROFILE%
+ECHO %%PUBLIC%%                       %PUBLIC%
+ECHO %%USERPROFILE%%                  %USERPROFILE%
+ECHO %%APPDATA%%                      %APPDATA%
+ECHO %%LOCALAPPDATA%%                 %LOCALAPPDATA%
+ECHO %%TEMP%%                         %TEMP%
+ECHO %%TMP%%                          %TMP%
+
+:: "将父路径和脚本名赋予变量以便于使用"
+SET parent=%~dp0
+SET me=%~n0
+
+:: "判断指令成功与否"
 :: SomeCommand.exe && ECHO SomeCommand.exe succeeded!
 :: SomeCommand.exe || ECHO SomeCommand.exe failed with return code %ERRORLEVEL%
 :: SomeCommand.exe || EXIT /B 1
 
-:: 输入输出重定向
+:: "输入输出重定向"
 :: SomeCommand.exe 1>NUL  2>NUL
 :: SomeCommand.exe 1>>NUL 2>>NUL
 :: SomeCommand.exe | AnotherCommand.exe
 
-:: 条件语句
-IF NOT EXIST "%0%" (
+:: "条件语句"
+IF NOT EXIST "%0" (
         ECHO Something is wrong.
         EXIT /B 1
 )
@@ -70,18 +118,20 @@ IF NOT DEFINED var (
         EXIT /B 1
 )
 
-IF NOT "%var%"=="hello, world" (
+IF NOT "%var%" == "hello, world" (
         ECHO Something is wrong.
         EXIT /B 1
 )
 
 :: 检查是否从资源管理器（双击）运行，若是则在退出前暂停，以供使用者查看输出
-CALL :pause_if_need
+CALL :pause_if_double_click
 EXIT /B 0
 
-:: 函数
-:: 通过 `CALL :pause_if_need arg1 arg2 ... argN` 调用
-:pause_if_need
+::             COMSPEC=C:\WINDOWS\system32\cmd.exe
+:: explorer CMDCMDLINE=C:\WINDOWS\system32\cmd.exe /C ""ScriptFullPath" "
+:: cmd      CMDCMDLINE="C:\Windows\System32\cmd.exe"
+:: cmder    CMDCMDLINE="C:\WINDOWS\SYSTEM32\cmd.exe" /K ""CmderInitPath" "
+:pause_if_double_click
         ECHO %CMDCMDLINE% | FINDSTR /L %COMSPEC% 1>NUL 2>NUL ^
                 && PAUSE
-EXIT /B 0
+        EXIT /B 0
