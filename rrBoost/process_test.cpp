a8de82e21eac1cb3b667155bc68b729220676f14
zhengrr <afoolsbag@gmail.com>
2020-01-15 18:37:31 +0800

    feat(rrBoost): 更新 Boost.Process 的示例

--- rrBoost/process_test.cpp
+++ rrBoost/process_test.cpp
@@ -1,12 +1,12 @@
-//===-- Process -------------------------------------------------*- C++ -*-===//
+//===-- Boost.Process -------------------------------------------*- C++ -*-===//
 ///
 /// \file
-/// \brief Process
+/// \brief Boost.Process
 /// \details Library to create processes in a portable way.
 ///
 /// \sa <https://boost.org/doc/libs/master/libs/process/>
 ///
-/// \version 2019-11-09
+/// \version 2020-01-15
 /// \since 2019-09-04
 /// \authors zhengrr
 /// \copyright Unlicense
@@ -43,10 +43,10 @@ TEST(process, named_pipe)
     // BUG: https://github.com/boostorg/process/issues/83
 
     ba::io_service ios;
-    bp::async_pipe ap{ ios, R"(\\.\pipe\rrBoostTest)" };
+    bp::async_pipe ap {ios, R"(\\.\pipe\rrBoostTest)"};
     ASSERT_TRUE(ap.is_open());
 
-    array<char, 256> buf{};
+    array<char, 256> buf {};
     ba::async_read(ap, ba::buffer(buf), [&](const bs::error_code &ec, size_t bytes_transferred) {
         (void)ec;
         (void)bytes_transferred;
@@ -54,6 +54,28 @@ TEST(process, named_pipe)
     });
     ios.run_for(1min);
 #endif
+}
+
+TEST(process, system)
+{
+    const auto ffmpeg_path = boost::process::search_path("ffmpeg");
+    if (ffmpeg_path.empty())
+        FAIL() << "The FFmpeg program not found.";
+
+    boost::process::ipstream ips; // input pipe stream
+    const int rc = boost::process::system(
+        ffmpeg_path,
+        "--help", "param2", "param3", "param...",
+        boost::process::std_in < boost::process::null,
+        boost::process::std_out > boost::process::null,
+        boost::process::std_err > ips);
+
+    string text, line;
+    while (std::getline(ips, line) && !line.empty())
+        text += line.append("\r\n");
+
+    if (rc != EXIT_SUCCESS)
+        FAIL() << text;
 }
 
 }
